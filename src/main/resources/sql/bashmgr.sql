a00817d3677afd33f3f7ff8b75093ddfa87caebb
dawnfrost <lingo.wxy@qq.com>
2017-12-22 14:45:02 +0800

    更新数据库

--- src/main/resources/sql/bashmgr.sql
+++ src/main/resources/sql/bashmgr.sql
@@ -1,6 +1,6 @@
 /*
 SQLyog Ultimate v12.3.3 (64 bit)
-MySQL - 5.7.18-log : Database - bashmgr
+MySQL - 5.7.16-log : Database - bashmgr
 *********************************************************************
 */
 
@@ -118,31 +118,55 @@ insert  into `bm_role_auth`(`ID`,`ROLE_ID`,`AUTH_ID`) values
 (2,1,2),
 (3,1,3);
 
+/*Table structure for table `bm_token` */
+
+DROP TABLE IF EXISTS `bm_token`;
+
+CREATE TABLE `bm_token` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `USER_ID` int(11) DEFAULT NULL,
+  `USER_AGENT` varchar(255) DEFAULT NULL,
+  `REMOTE_HOST` varchar(255) DEFAULT NULL,
+  `REFRESH_TOKEN` varchar(64) DEFAULT NULL,
+  `REFRESH_TOKEN_CREATE_TIME` datetime DEFAULT NULL,
+  `REFRESH_TOKEN_EXPIRE_TIME` datetime DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+/*Data for the table `bm_token` */
+
+insert  into `bm_token`(`ID`,`USER_ID`,`USER_AGENT`,`REMOTE_HOST`,`REFRESH_TOKEN`,`REFRESH_TOKEN_CREATE_TIME`,`REFRESH_TOKEN_EXPIRE_TIME`) values 
+(1,1,'PostmanRuntime/7.1.1',NULL,'0a24515d-bc0f-4b18-b84e-7fd6b6d7c89f','2017-12-19 18:30:33','2018-01-03 18:30:33'),
+(2,2,'PostmanRuntime/7.1.1',NULL,'2d77941f-b65a-44be-b447-14375ef172b3','2017-12-19 18:29:08','2018-01-03 18:29:08');
+
 /*Table structure for table `bm_user` */
 
 DROP TABLE IF EXISTS `bm_user`;
 
 CREATE TABLE `bm_user` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `MARK_CODE` varchar(255) DEFAULT NULL,
   `USER_NAME` varchar(64) DEFAULT NULL,
-  `PASSWORD` varchar(255) DEFAULT NULL,
+  `PASSWORD` varchar(128) DEFAULT NULL,
   `EMAIL` varchar(128) DEFAULT NULL,
-  `PHONE` varchar(64) DEFAULT NULL,
-  `DISPLAY_NAME` varchar(64) DEFAULT NULL,
-  `DESCRIPTION` varchar(255) DEFAULT NULL,
+  `PHONE` varchar(32) DEFAULT NULL,
+  `DISPLAY_NAME` varchar(32) DEFAULT NULL,
   `CREATE_TIME` datetime NOT NULL,
   `LAST_LOGIN_TIME` datetime DEFAULT NULL,
   `IS_ENABLE` tinyint(4) DEFAULT NULL,
   `STATUS` int(11) DEFAULT NULL,
   `EXPIRE_TIME` datetime NOT NULL,
   `IS_LOCKED` tinyint(4) DEFAULT NULL,
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `USER_NAME_index` (`USER_NAME`) USING BTREE,
+  UNIQUE KEY `MARK_CODE_index` (`MARK_CODE`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 /*Data for the table `bm_user` */
 
-insert  into `bm_user`(`ID`,`USER_NAME`,`PASSWORD`,`EMAIL`,`PHONE`,`DISPLAY_NAME`,`DESCRIPTION`,`CREATE_TIME`,`LAST_LOGIN_TIME`,`IS_ENABLE`,`STATUS`,`EXPIRE_TIME`,`IS_LOCKED`) values 
-(1,'aad','2312444',NULL,NULL,NULL,NULL,'2017-12-17 13:43:33',NULL,1,0,'2017-12-30 13:43:42',0);
+insert  into `bm_user`(`ID`,`MARK_CODE`,`USER_NAME`,`PASSWORD`,`EMAIL`,`PHONE`,`DISPLAY_NAME`,`CREATE_TIME`,`LAST_LOGIN_TIME`,`IS_ENABLE`,`STATUS`,`EXPIRE_TIME`,`IS_LOCKED`) values 
+(1,'8aab883392c787a8c62033d22d72e4fe271e4957fcc37305adf25de34362cbf2','root','password','lingo.wxy@qq.com','15866682816','wxy41','2017-12-17 13:43:33',NULL,1,0,'2017-12-30 13:43:42',0),
+(2,'8aab883392c787a8c62033d22d72e4fe271e4957fcc37305ad565de34362cbf2','user','password','1111','22333','wxy44','2017-12-19 17:48:38',NULL,1,0,'2017-12-30 17:48:51',0);
 
 /*Table structure for table `bm_user_group` */
 
@@ -153,12 +177,13 @@ CREATE TABLE `bm_user_group` (
   `USER_ID` int(11) DEFAULT NULL,
   `GROUP_ID` int(11) DEFAULT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 /*Data for the table `bm_user_group` */
 
 insert  into `bm_user_group`(`ID`,`USER_ID`,`GROUP_ID`) values 
-(1,1,1);
+(1,1,1),
+(2,2,3);
 
 /*Table structure for table `bm_user_role` */
 
